be12a8561631fb04212451257955b36e1c550505
Kai Haskell <me@kaihaskell.com>
2020-11-10 19:22:21 -0500

    setup reducer

--- travel-planner/src/App.js
+++ travel-planner/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-
+import {reducer} from "./Util/reducer";
 //Components
 import Home from "./Components/Home.js"
 import Login from "./Components/Login.js"
@@ -8,17 +8,30 @@ import Login from "./Components/Login.js"
 //Styles
 import './App.css';
 
-
+export const AuthContext = React.createContext();
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+} 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <Router>
-      <Switch>
-        <Route exact path ="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-      </Router>
-    </div>
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch
+      }}
+    >
+      <div className="App">
+        <Router>
+        <Switch>
+          <Route exact path ="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
